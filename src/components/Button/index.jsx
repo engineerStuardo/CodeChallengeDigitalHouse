187cb77d62a5a882f4dd12c5174823597bc03b5f
engineerStuardo <ingeniero.stuardo@gmail.com>
2023-04-05 12:50:29 -0600

    removing unused import

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
